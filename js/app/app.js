8642843701e231988aec58845459d656ef9784b8
Brant Wills <brantwills@gmail.com>
2016-04-30 18:47:03 -0400

    Development Sprint 16

--- js/app/app.js
+++ js/app/app.js
@@ -7,4 +7,18 @@ window.tns = window.tns || {}
 
 
 // delare our copy button
-new Clipboard('.btn-copy');
+var clipboard = new Clipboard('.btn-copy');
+
+
+// copy success
+clipboard.on('success', function(e) {
+	document.getElementById('copyText').innerHTML = "Copied!"	
+	window.scrollTo(0, 0);
+});
+
+
+// copy error
+clipboard.on('error', function(e) {
+	document.getElementById('copyText').innerHTML = "Error Copying.."	
+	window.scrollTo(0, 0);
+});
